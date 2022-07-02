a8a0d798b0fe45e464ab966f9d3b53a4b0c5bf48
OdellBIII <blackmoniii.odell@gmail.com>
2022-07-02 17:30:18 -0400

    Added an API call to retrieve email

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { OAuth2Client } = require('google-auth-library');
 const {google} = require('googleapis');
+const gmail = google.gmail('v1');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const oauth2Cleint = new google.auth.OAuth2(
+const OAuth2Client = new google.auth.OAuth2(
     // TODO: Have Client ID and Client secret be read from a file
     // Client ID
     '122228461155-l5dgh6ta5qkmlub17gg2bc7s71k31dt1.apps.googleusercontent.com',
@@ -34,11 +36,28 @@ app.get('/', (req, res) => {
     res.redirect(oauthUrl);
 });
 
+app.get('/auth', (req, res) => {
+
+});
+
 app.get('/welcome', (req, res) => {
     
     const code = req.query.code;
     console.log(code);
-    res.send("Welcome to EVC!");
+
+    const {tokens} = await OAuth2Client.getToken(code);
+    OAuth2Client.setCredentials(tokens);
+    google.options({OAuth2Client});
+    const result = await gmail.users.getProfile({
+        userId: 'me'
+    });
+    console.log(result.emailAddress)
+    res.sendFile("welcome.html");
+});
+
+app.get('/thankyou', (req, res) =>{
+
+    console.log(req.query.email);
 });
 
 app.listen(port, () => {
